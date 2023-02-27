f861c7d763219ca6dfed09b44f8056d3db7bcb4c
mooney0129 <mooneydev9001@gmail.com>
2023-02-27 15:02:05 -0500

    update

--- frontend/src/CustomerCreateUpdate.js
+++ frontend/src/CustomerCreateUpdate.js
@@ -1,111 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import CustomersService from './CustomersService';
 
 const customersService = new CustomersService();
 
-class CustomerCreateUpdate extends Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+function CustomerCreateUpdate(props)
+{
+  const initialState = {
+    pk:"",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    address: "",
+    description: "",
+  };
 
-      componentDidMount(){
-        const {  params } = this.props;
-        if(params && params.pk)
-        {
-          customersService.getCustomer(params.pk).then((c)=>{
-            this.firstName.current.value = c.first_name;
-            this.lastName.current.value = c.last_name;
-            this.email.current.value = c.email;
-            this.phone.current.value = c.phone;
-            this.address.current.value = c.address;
-            this.description.current.value = c.description;
-          })
-        }
-      }
+  const [crud, setCrud] = useState(initialState);
 
-      handleCreate(){
-        customersService.createCustomer(
-          {
-            "first_name": this.firstName.current.value,
-            "last_name": this.lastName.current.value,
-            "email": this.email.current.value,
-            "phone": this.phone.current.value,
-            "address": this.address.current.value,
-            "description": this.description.current.value
-        }          
-        ).then((result)=>{
-          alert("Customer created!");
-        }).catch(()=>{
-          alert('There was an error! Please re-check your form.');
-        });
-      }
-      handleUpdate(pk){
-        customersService.updateCustomer(
-          {
-            "pk": pk,
-            "first_name": this.firstName.current.value,
-            "last_name": this.lastName.current.value,
-            "email": this.email.current.value,
-            "phone": this.phone.current.value,
-            "address": this.address.current.value,
-            "description": this.description.current.value
-        }          
-        ).then((result)=>{
-          console.log(result);
-          alert("Customer updated!");
-        }).catch(()=>{
-          alert('There was an error! Please re-check your form.');
-        });
-      }
-      handleSubmit(event) {
-        const {  params } = this.props;
-        console.log(params);
-        if(params && params.pk){
-          this.handleUpdate(params.pk);
-        }
-        else
-        {
-          this.handleCreate();
-        }
+  function handleChange(event) 
+  {
+    setCrud({ ...crud, [event.target.name]: event.target.value });
+  }
 
-        event.preventDefault();
-      }
-    
-      render() {
-        return (
-          <form >
-          <div className="form-group">
-            <label>
-              First Name:</label>
-              <input className="form-control" type="text" ref='firstName' />
-            
-            <label>
-              Last Name:</label>
-              <input className="form-control" type="text" ref='lastName'/>
-            
-            <label>
-              Phone:</label>
-              <input className="form-control" type="text" ref='phone' />
-            
-            <label>
-              Email:</label>
-              <input className="form-control" type="text" ref='email' />
-            
-            <label>
-              Address:</label>
-              <input className="form-control" type="text" ref='address' />
-            
-            <label>
-              Description:</label>
-              <textarea className="form-control" ref='description' ></textarea>
-              
+  function handleCreate()
+  {
+    customersService.createCustomer(crud)
+    .then((result)=>{
+      alert("Customer created!");
+    }).catch(()=>{
+      alert('There was an error! Please re-check your form.');
+    });
+  }
+  function handleUpdate(pk)
+  {
+    customersService.updateCustomer(crud    
+    ).then((result)=>{
+      console.log(result);
+      alert("Customer updated!");
+    }).catch(()=>{
+      alert('There was an error! Please re-check your form.');
+    });
+  }
+  function handleSubmit(event)
+  {
+    console.log(props);
+    console.log(crud);
+    handleCreate();
+    event.preventDefault();
+  }
+  return (
+    <form>
+    <div className="form-group">
+      <label>
+        First Name:</label>
+        <input className="form-control" type="text" name="firstName" value={crud.firstName} onChange={handleChange}/>
+      
+      <label>
+        Last Name:</label>
+        <input className="form-control" type="text" name="lastName" value={crud.lastName} onChange={handleChange}/>
+      
+      <label>
+        Phone:</label>
+        <input className="form-control" type="text" name="email" value={crud.email} onChange={handleChange} />
+      
+      <label>
+        Email:</label>
+        <input className="form-control" type="text" name="phone" value={crud.phone} onChange={handleChange} />
+      
+      <label>
+        Address:</label>
+        <input className="form-control" type="text" name="description" value={crud.description} onChange={handleChange} />
+      
+      <label>
+        Description:</label>
+        <textarea className="form-control"></textarea>
+        
 
-            <input className="btn btn-primary" type="submit" value="Submit" onClick={this.handleSubmit}/>
-            </div>
-          </form>
-        );
-      }  
+      <input className="btn btn-primary" type="button" value="Submit" onClick={handleSubmit}/>
+      </div>
+    </form>
+  );
 }
 
-export default CustomerCreateUpdate;  
+
+export default CustomerCreateUpdate;
